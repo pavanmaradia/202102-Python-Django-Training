c39b31f2caf49956ac95e1ab433e57d00fb3189b
parthpatel <pavan.maradia@yahoo.com>
2021-02-04 22:41:30 +0530

    Session 02 completed with variable and basic data type of Python

--- sessions/session_02.py
+++ sessions/session_02.py
@@ -8,3 +8,44 @@ Data type:
     float
     None
 """
+
+# call by value
+# call by reference / call by object
+
+VAR_INT_1 = int()
+VAR_INT_2 = int()
+VAR_INT_3 = 2
+# print(VAR_INT, type(VAR_INT))
+#
+# print('VAR_INT_1: ', VAR_INT_1, type(VAR_INT_1), id(VAR_INT_1))
+# print('VAR_INT_2: ', VAR_INT_2, type(VAR_INT_2), id(VAR_INT_2))
+# print('VAR_INT_3: ', VAR_INT_3, type(VAR_INT_3), id(VAR_INT_3))
+
+VAR_STR = str()
+# print(VAR_STR, type(VAR_STR))
+
+VAR_INT = "Changed value"
+# print(VAR_INT, type(VAR_INT))
+
+VAR_BOOLEAN_TRUE = True
+VAR_BOOLEAN_FALSE = False
+VAR_BOOLEAN = True
+
+# print(VAR_BOOLEAN, type(VAR_BOOLEAN), id(VAR_BOOLEAN))
+# print(VAR_BOOLEAN_TRUE, type(VAR_BOOLEAN_TRUE), id(VAR_BOOLEAN_TRUE))
+# print(VAR_BOOLEAN_FALSE, type(VAR_BOOLEAN_FALSE), id(VAR_BOOLEAN_FALSE))
+
+VAR_FLOAT_01 = 3.00
+VAR_FLOAT_02 = 3
+
+# print(VAR_FLOAT_01, type(VAR_FLOAT_01), id(VAR_FLOAT_01))
+# print(VAR_FLOAT_02, type(VAR_FLOAT_02), id(VAR_FLOAT_02))
+
+VAR_NONE = None
+print(VAR_NONE, type(VAR_NONE), id(VAR_NONE))
+
+
+
+
+
+
