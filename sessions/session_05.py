5b5be61390cafa99de673dc7604fc5b43094a8c6
parthpatel <pavan.maradia@yahoo.com>
2021-02-08 20:20:15 +0530

    Session 05: Initial Commit

---
+++ sessions/session_05.py
@@ -0,0 +1,12 @@
+"""
+Looping statements
+    while
+    for
+    for else
+
+Assignments:
+    1. Print 1-10 using for loop
+    2. Print 1-30 odd numbers
+    3. Print table of 10
+    4. Print table of 17
+"""
