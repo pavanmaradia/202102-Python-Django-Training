78c6f47fb1c7de2c86ec7dc9336c828c060e0222
parthpatel <pavan.maradia@yahoo.com>
2021-02-08 20:02:22 +0530

    Session 04: Initial Commit

---
+++ sessions/session_04.py
@@ -0,0 +1,16 @@
+"""
+Operators
+    Arithmetic ( +, -, *, /, %, **, // )
+    Comparison ( ==, !=, <, >, <=, >= )
+    Assignment ( =, +=, -=, *=, /=, %=, **=, //= )
+    Logical ( and, or, not )
+    Membership ( in, not in )
+    Identity ( is, is not)
+
+Conditional/Control Statement
+    if
+    if-else
+    if-elif-else
+    nested if-else
+"""
+
