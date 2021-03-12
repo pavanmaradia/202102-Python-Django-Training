f7f62534a61bdeaa2b9363205fb1d7df7dbdadc6
parthpatel <pavan.maradia@yahoo.com>
2021-03-12 21:39:21 +0530

    session_23: completed. Django models, makemigrations, migrate, sqlmigrate, createsuperuser and admin model configuration

--- emp_directory/emp/models.py
+++ emp_directory/emp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Employee(models.Model):
+    id = models.UUIDField(primary_key=True)
+    first_name = models.CharField(max_length=128, null=True)
+    last_name = models.CharField(max_length=128, blank=True)
+    age = models.IntegerField()
+    address = models.TextField(max_length=1024)
+
+    def __str__(self):
+        return F"{self.first_name} {self.last_name}"
+
+
